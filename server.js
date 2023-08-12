8dd74f6ce0d995fa7906c868f6347499aae85ff9
Jorgetorreslopez <usda4ever@gmail.com>
2023-08-12 12:00:08 -0400

    Adjusted Code/ Installed Sockets.io

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('./config/database')
 const app = require('./app-server');
 const PORT = process.env.PORT || 8000;
 
+
+
 app.listen(PORT, () => {
     console.log(`API Listening on port ${PORT}`);
 });
